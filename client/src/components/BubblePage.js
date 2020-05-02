7b10a1e08b8e14de38cb4566e6d20ae04acdb3de
webbmaxwell <webbwrites@gmail.com>
2020-05-01 21:46:42 -0600

    colors and bubbles render correctly.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,15 +4,17 @@ import axios from "axios";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-  axios
+  axiosWithAuth()
     .get("http://localhost:5000/api/colors")
     .then(res => {
-      console.log(res)
-      setColorList(res)
+    //  console.log(res.data)
+      setColorList(res.data)
     })
     .catch(err => console.error(err));
 
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -7,7 +7,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  //console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
