020e16a5af70569507c9d4bfad30391e284b64b5
webbmaxwell <webbwrites@gmail.com>
2020-05-03 21:31:09 -0600

    Continued adding detail to AddForm.

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { initialColor } from "./ColorList";
 
 const AddForm = () => {
+
+  const [newColor, setNewColor] = useState(initialColor);
+
   const addColor = () => {
     axiosWithAuth()
       .post("http://localhost:5000/api/colors")
@@ -12,8 +16,32 @@ const AddForm = () => {
       })
   }
   return (
-    <>
+    <div>
       <form onSubmit={addColor}>
-    </>
+        <legend>new color</legend>
+        <label>
+          colorname:
+          <input
+            onChange={e =>
+              setNewColor({
+                ...newColor,
+                color: e.target.value
+              })
+            }
+            value={newColor.color}
+          />
+        </label>
+        <label>
+          hex code:
+          <input
+            onChange={e =>
+              setNewColor({
+                ...newColor,
+                code: { hex: e.target.value }
+              })
+            }
+          />
+        </label>
+    </div>
   )
 }
