25334913531d1b50fb70e27a524d1a4c9e6391c3
webbmaxwell <webbwrites@gmail.com>
2020-05-03 19:18:14 -0600

    Started filling out .put request inside ColorList.js

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -16,11 +16,18 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = {e, id, newColor} => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${id}`, newColor)
+      .then(res => {
+        console.log(res)
+
+      })
+      .catch(err => console.error(err))
   };
 
   const deleteColor = color => {
